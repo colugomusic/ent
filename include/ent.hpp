86f598e8e0a761adf8cc08bd4aaf3dc54dc5c7db
Colugo <colugomusic@gmail.com>
2024-07-28 16:05:30 +0100

    Update ent.hpp

--- include/ent.hpp
+++ include/ent.hpp
@@ -301,111 +301,69 @@ private:
 	std::vector<size_t> free_indices_;
 };
 
-/*
-template <size_t BlockSize, typename T>
-struct block_array {
-	auto swap(size_t a, size_t b) -> void {
-		std::swap(data_[a], data_[b]);
-	}
-	template <typename VisitFn>
-	auto visit(const VisitFn& visit_fn, size_t n) -> void {
-		for (size_t i = 0; i < n; ++i) {
-			visit_fn(data_[i]);
-		}
-	}
-	auto operator[](size_t index) -> T&             { return data_[index]; }
-	auto operator[](size_t index) const -> const T& { return data_[index]; }
-private:
-	std::array<T, BlockSize> data_;
+template <typename KeyType>
+struct sparse_map_handle {
+	KeyType key;
+	size_t index;
 };
 
-template <size_t BlockSize, typename... Ts>
-struct block {
-	block() {
-		std::iota(index_map_.begin(), index_map_.end(), 0);
+template <size_t BlockSize, template <typename Key, typename Value> typename MapImpl, typename KeyType, typename... Ts>
+struct sparse_map {
+	using key_type    = KeyType;
+	using handle_type = sparse_map_handle<key_type>;
+	[[nodiscard]]
+	auto add(key_type key) -> handle_type {
+		const auto index    = table.add();
+		key_to_index[key]   = index;
+		index_to_key[index] = key;
+		return {key, index};
 	}
-	auto add() -> size_t {
-		const auto idx = size_;
-		(reset<Ts>(idx), ...);
-		size_++;
-		return idx;
+	[[nodiscard]]
+	auto exists(key_type key) const -> bool {
+		return key_to_index.find(key) != key_to_index.end();
 	}
-	auto clear() -> void {
-		size_ = 0;
+	[[nodiscard]]
+	auto is_alive(size_t index) const -> bool {
+		return index_to_key.find(index) != index_to_key.end();
+	}
+	auto get_living_elements(std::vector<handle_type>* out) const -> void {
+		out->clear();
+		for (const auto [key, index] : key_to_index) {
+			out->push_back({key, index});
+		}
+	}
+	auto get_living_elements(std::vector<size_t>* out) const -> void {
+		table.get_living_elements(out);
+	}
+	auto erase(handle_type handle) -> void {
+		table.erase(handle.index);
+		key_to_index.erase(handle.key);
+		index_to_key.erase(handle.index);
+	}
+	auto erase(key_type key) -> void {
+		erase({key, key_to_index[key]});
 	}
 	auto erase(size_t index) -> void {
-		(std::get<block_array<BlockSize, Ts>>(data_).swap(index_map_[index], size_), ...);
-		std::swap(index_map_[index], index_map_[size_]);
-		size_--;
-	}
-	template <typename T, typename VisitFn>
-	auto visit(const VisitFn& visit_fn) -> void {
-		std::get<block_array<BlockSize, T>>(data_).visit(visit_fn, size_);
-	}
-	auto size() const -> size_t { return size_; }
-	template <typename T> auto set(size_t index, T value) -> void                { get<T>(index) = std::move<T>(value); }
-	template <typename T> [[nodiscard]] auto get(size_t index) -> T&             { return std::get<block_array<BlockSize, T>>(data_)[index_map_[index]]; }
-	template <typename T> [[nodiscard]] auto get(size_t index) const -> const T& { return std::get<block_array<BlockSize, T>>(data_)[index_map_[index]]; }
-private:
-	template <typename T> auto reset(size_t index) -> void { get<T>(index) = T{}; }
-	using Tuple = std::tuple<block_array<BlockSize, Ts>...>;
-	Tuple data_;
-	size_t size_ = 0;
-	std::array<size_t, BlockSize> index_map_;
-};
-
-template <size_t BlockSize, typename... Ts>
-struct block_table {
-	auto add() -> size_t {
-		size_t block_index = 0;
-		for (auto& block : blocks_) {
-			if (block.size() < BlockSize) {
-				return block_index * BlockSize + block.add();
-			}
-		}
-		return (blocks_.size() * BlockSize) + add_block().add();
-	}
-	auto erase(size_t elem_index) -> void {
-		const auto block_index = elem_index / BlockSize;
-		const auto array_index = elem_index % BlockSize;
-		get_block(elem_index).erase(array_index);
+		erase({index_to_key[index], index});
 	}
 	auto clear() -> void {
-		for (auto& block : blocks_) {
-			block.clear();
-		}
+		table.clear();
+		key_to_index.clear();
+		index_to_key.clear();
 	}
 	auto size() const -> size_t {
-		return std::accumulate(blocks_.begin(), blocks_.end(), size_t(0), [](size_t acc, const auto& block) {
-			return acc + block.size();
-		});
+		return key_to_index.size();
 	}
-	template <typename T, typename VisitFn>
-	auto visit(const VisitFn& visit_fn) -> void {
-		for (auto& block : blocks_) {
-			block.visit<T>(visit_fn);
-		}
-	}
-	template <typename T> auto set(size_t elem_index, T value) -> void                { get_block(elem_index).set(elem_index, std::move<T>(value)); }
-	template <typename T> [[nodiscard]] auto get(size_t elem_index) -> T&             { return get_block(elem_index).get<T>(elem_index % BlockSize); }
-	template <typename T> [[nodiscard]] auto get(size_t elem_index) const -> const T& { return get_block(elem_index).get<T>(elem_index % BlockSize); }
+	template <typename T> auto set(size_t index, T&& value) -> T&                      { return table.set(index, std::forward<T>(value)); }
+	template <typename T> auto set(handle_type handle, T&& value) -> T&                { return table.set(handle.index, std::forward<T>(value)); }
+	template <typename T> [[nodiscard]] auto get(size_t index) -> T&                   { return table.get(index); }
+	template <typename T> [[nodiscard]] auto get(size_t index) const -> const T&       { return table.get(index); }
+	template <typename T> [[nodiscard]] auto get(handle_type handle) -> T&             { return table.get(handle.index); }
+	template <typename T> [[nodiscard]] auto get(handle_type handle) const -> const T& { return table.get(handle.index); }
 private:
-	auto add_block() -> decltype(auto) {
-		blocks_.emplace_back();
-		return blocks_.back();
-	}
-	auto get_block(size_t elem_index) -> block<BlockSize, Ts...>&             {
-		auto pos = blocks_.begin();
-		std::advance(pos, elem_index / BlockSize);
-		return *pos;
-	}
-	auto get_block(size_t elem_index) const -> const block<BlockSize, Ts...>& {
-		auto pos = blocks_.begin();
-		std::advance(pos, elem_index / BlockSize);
-		return *pos;
-	}
-	std::list<block<BlockSize, Ts...>> blocks_;
+	MapImpl<key_type, size_t> key_to_index;
+	MapImpl<size_t, key_type> index_to_key;
+	sparse_table<BlockSize, Ts...> table;
 };
-*/
 
 } // ent
